3bf8fc01cf70dd476a95e0fb1d49ef212815a2ee
gharesh14 <135325288+gharesh14@users.noreply.github.com>
2025-08-24 17:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,8 +131,9 @@ def predict():
 # -------------------- Run App -------------------- #
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
